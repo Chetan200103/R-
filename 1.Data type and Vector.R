8e6357e8f9be752b34d7619fd733c09703738304
Chetan Habib <116337670+Chetan200103@users.noreply.github.com>
2023-01-19 18:07:55 +0530

    Add files via upload

---
+++ 1.Data type and Vector.R
@@ -0,0 +1,57 @@
+##Variable Assignment
+# = or <- means assign
+x <- 1   # or 
+x = 20
+
+print(x)
+
+x
+
+###Function and c is combining values into a vector or list 
+c(10,20,30)   
+
+a = c(10,20,30)
+
+##to understand  what is c function
+help(c)   # r Expert or guidance provider
+
+##Numeric
+x = 10.5       # Float number 
+x              # print the value of x 
+
+###To check the ClASS OF X
+class(x)       # print the class name of x 
+
+
+k = 10 ## Integer 
+k      
+
+class(k)       # print the class name of k 
+
+  
+####tO CONFIRM whether it is integer or not    
+is.integer(k)  # is k an integer? 
+  
+y = as.integer(k) 
+
+# as = convert 
+#.integer = after . data type 
+
+y              # print the value of y 
+
+class(y)       # print the class name of y 
+
+is.integer(y)  # is y an integer?   
+
+###complex Value
+z = 1 + 2i     # create a complex number 
+z              # print the value of z 
+
+class(z)       # print the class name of z 
+
+
+x = as.character(2.1) 
+x              # print the character string 
+class(x)       # print the class name of x 
+
+is.character(x)
